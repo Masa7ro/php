9538551f08598efabac4a077d2226095aef34ab0
Masa7ro <132319503+Masa7ro@users.noreply.github.com>
2023-06-24 09:39:48 +0900

    add readme.md contents

---
+++ input.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>インプット</title>
+</head>
+
+<body>
+
+<div style= "background:#0000ff;color:#ffffff;">アンケートフォーム/div>
+    <form action="write.php" method="post">
+        お名前: <input type="text" name="name">
+        出身: <input type="text" name="birthPlace">
+        血液型:<input type="text" name ="bloodtype">
+        <input type="submit" value="送信">
+    </form>
+</body>
+
+</html>
